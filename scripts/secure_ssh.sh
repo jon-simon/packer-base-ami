4d9fa0e2e0fad17ebb2151d4515d82388e21bfc1
Jonathan Simon <jonathan.simon@meetcircle.com>
2018-12-28 19:50:14 -0600

    adding -a (append) flag to the tee commands in secure ssh script

--- scripts/secure_ssh.sh
+++ scripts/secure_ssh.sh
@@ -4,7 +4,7 @@ CONFIG_FILE=/etc/ssh/ssh_config
 
 ## Adding additional settings to ssh_config to satisfy dev-sec ssh hardening compliance
 echo "Setting additional ssh_config settings to satisfy compliance checks"
-echo "" | tee $CONFIG_FILE
-echo "RhostsRSAAuthentication no" | tee $CONFIG_FILE
-echo "RSAAuthentication yes" | tee $CONFIG_FILE
-echo "UseRoaming no" | tee $CONFIG_FILE
+echo "" | tee -a $CONFIG_FILE
+echo "RhostsRSAAuthentication no" | tee -a $CONFIG_FILE
+echo "RSAAuthentication yes" | tee -a $CONFIG_FILE
+echo "UseRoaming no" | tee -a $CONFIG_FILE
