d76a8b983da523705dea8fd520e4b713ad34ce26
Jonathan Simon <jonathan.simon@meetcircle.com>
2019-01-11 15:32:11 -0800

    rerunning with new build container to fix packer issue

--- setenv.sh
+++ setenv.sh
@@ -4,6 +4,5 @@
 ## build manually.  When using CI/CD tool, set ENV variables
 ## in the tool.
 
-
 for a in {'AWS_PROFILE','AWS_REGION','AMI_PREFIX','AMI_NAME','BUILD_REGION'}; do echo $a? && read res && export $a=$res; done
 
