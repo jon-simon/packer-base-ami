08cbab77cc10b55ce653016a1f526ac29df847fa
Jonathan Simon <jonathan.simon@meetcircle.com>
2019-01-11 16:04:44 -0800

    removing the ssh hardening for now because I'm unable to ssh at all

--- scripts/compliance.sh
+++ scripts/compliance.sh
@@ -7,4 +7,4 @@ curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P inspec
 inspec exec https://github.com/dev-sec/linux-baseline --no-distinct-exit ## need to add this flag to handle 'skipped' tests.
 
 # Run inspec ssh-baseline tests
-inspec exec https://github.com/dev-sec/ssh-baseline --no-distinct-exit ## need to add this flag to handle 'skipped' tests.
+# inspec exec https://github.com/dev-sec/ssh-baseline --no-distinct-exit ## need to add this flag to handle 'skipped' tests.
