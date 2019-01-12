5fcb30befc1b2607f513a0ae17374a24437a35a3
Jonathan Simon <jonathan.simon@meetcircle.com>
2019-01-12 12:45:10 -0800

    adding back ssh hardening

--- scripts/secure_ssh.sh
+++ scripts/secure_ssh.sh
@@ -3,8 +3,8 @@
 CONFIG_FILE=/etc/ssh/ssh_config
 
 ## Adding additional settings to ssh_config to satisfy dev-sec ssh hardening compliance
-echo "Setting additional ssh_config settings to satisfy compliance checks"
-echo "" | tee -a $CONFIG_FILE
-echo "RhostsRSAAuthentication no" | tee -a $CONFIG_FILE
-echo "RSAAuthentication yes" | tee -a $CONFIG_FILE
-echo "UseRoaming no" | tee -a $CONFIG_FILE
+# echo "Setting additional ssh_config settings to satisfy compliance checks"
+# echo "" | tee -a $CONFIG_FILE
+# echo "RhostsRSAAuthentication no" | tee -a $CONFIG_FILE
+# echo "RSAAuthentication yes" | tee -a $CONFIG_FILE
+# echo "UseRoaming no" | tee -a $CONFIG_FILE
