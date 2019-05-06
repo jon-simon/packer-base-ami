4bc2168ce9773a195fd80acfe0615215e78dd4b9
Jonathan Simon <jonathan.simon@meetcircle.com>
2019-05-06 15:45:02 -0700

    adding chef license accept for inspec

--- scripts/compliance.sh
+++ scripts/compliance.sh
@@ -3,6 +3,8 @@
 # # Installing InSpec
 curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P inspec
 
+inspec version --chef-license=accept-silent                              ## accepting the new chef license
+
 # Run inspec linux-baseline tests
 inspec exec https://github.com/dev-sec/linux-baseline --no-distinct-exit ## need to add this flag to handle 'skipped' tests.
 
